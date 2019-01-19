4260f8d3857a4e83880e064c6c6086783ce948ca
Dariyo Ignatov <darioignatov72@gmail.com>
2019-01-19 22:35:16 +0200

    Solution: needs refactoring

--- Professional modules - (1)module/JS Core - (3)courses/JS Fundamentals - first course/Labs/02_Format-the-Text/solution.js
+++ Professional modules - (1)module/JS Core - (3)courses/JS Fundamentals - first course/Labs/02_Format-the-Text/solution.js
@@ -1,4 +1,39 @@
-function solve(){
-  console.log(`This is a test`);
-  
+function solve() {
+  let inputText = document.getElementById('input').textContent;
+  let outputParent = document.getElementById('output');
+  let splitted = inputText.split('.');
+
+
+  for (let i = 0; i < splitted.length; i++) {
+    let currentSentence = splitted[i];
+    if (currentSentence.length < 1) {
+      let index = splitted.indexOf(currentSentence);
+      splitted.splice(index, 1);
+    }
+  }
+
+  if (splitted.length <= 3) {
+    let newParagraph = document.createElement('p');
+    newParagraph.innerText = inputText;
+    outputParent.appendChild(newParagraph);
+  } else {
+
+    let check = 1;
+    while (check === 1) {
+      if (splitted.length > 3) {
+        let newParagraph = document.createElement('p');
+        let paragraphSentences = splitted.splice(0, 3).join('.');
+        newParagraph.innerText = paragraphSentences;
+        outputParent.appendChild(newParagraph);
+      }else {
+        let lastNewParagraph = document.createElement('p');
+        let lastParagraphSentences = splitted.join('.');
+        lastNewParagraph.innerText = lastParagraphSentences;
+        outputParent.appendChild(lastNewParagraph);
+        check = 0;
+      }
+    }
+
+  }
+
 }
