a510c8b242c7665a8c18ffaccecbf5d83b7cdb8d
Dariyo Ignatov <darioignatov72@gmail.com>
2019-01-19 17:40:22 +0200

    Solution

--- Professional modules - (1)module/JS Core - (3)courses/JS Fundamentals - first course/Labs/01_Articles-List/solution.js
+++ Professional modules - (1)module/JS Core - (3)courses/JS Fundamentals - first course/Labs/01_Articles-List/solution.js
@@ -1,3 +1,26 @@
 function solve() {
-	//TODO
+	let titleInputText = document.getElementById('createTitle').value;
+	let textInputText = document.getElementById('createContent').value;
+
+	if (textInputText && titleInputText) {
+
+		let createH3 = document.createElement('h3');
+		createH3.textContent = titleInputText;
+		let createParagraph = document.createElement('p');
+		createParagraph.textContent = textInputText;
+
+		let createArticleEl = document.createElement('article');
+		createArticleEl.appendChild(createH3);
+		createArticleEl.appendChild(createParagraph);
+
+		let getSection = document.getElementById('articles');
+		getSection.appendChild(createArticleEl);
+	}
+
+	let titleClear = document.getElementById('createTitle');
+	let textClear = document.getElementById('createContent');
+	titleClear.value = "";
+	textClear.value = "";
+
+
 }
