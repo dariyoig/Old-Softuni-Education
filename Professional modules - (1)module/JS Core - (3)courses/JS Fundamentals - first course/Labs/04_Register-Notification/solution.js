e3510898caaa486b47bb6448217755864a9afe79
Dariyo Ignatov <darioignatov72@gmail.com>
2019-01-20 21:04:34 +0200

    Solution added: Not Judge ready

--- Professional modules - (1)module/JS Core - (3)courses/JS Fundamentals - first course/Labs/04_Register-Notification/solution.js
+++ Professional modules - (1)module/JS Core - (3)courses/JS Fundamentals - first course/Labs/04_Register-Notification/solution.js
@@ -1,4 +1,27 @@
+function register() {
+  let username = document.getElementById('username').value;
+  let email = document.getElementById('email').value;
+  let password = document.getElementById('password').value;
 
- function register() {
-   //TODO
- }
+  let regex = /(.+)@(.+).(com|bg)/gm;
+
+  let isEmailValid = regex.test(email);
+
+  if (isEmailValid && username.length > 0 && password.length > 0) {
+    let cryptoPassword = `Password: ${'*'.repeat(password.length)}`;
+    let result = document.getElementById('result');
+    let heading = document.createElement('h1');
+    let usernameMsg = `Username: ${username}`;
+    let emailMsg = `Email: ${email}`;
+    heading.innerText = `Successful Registration!`;
+
+    result.appendChild(heading);
+    result.insertAdjacentHTML('beforeend', `${usernameMsg}`);
+    result.insertAdjacentHTML('beforeend', '<br>');
+    result.insertAdjacentHTML('beforeend', `${emailMsg}`);
+    result.insertAdjacentHTML('beforeend', `<br>`);
+    result.insertAdjacentHTML('beforeend', `${cryptoPassword}`);
+
+  }
+
+}
