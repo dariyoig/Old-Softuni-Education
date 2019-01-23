7bc6605558d1dfb1be0ee022b7b90eed1051fe60
Dariyo Ignatov <darioignatov72@gmail.com>
2019-01-23 18:53:21 +0200

    Solutions added

--- Professional modules - (1)module/JS Core - (3)courses/JS Fundamentals - first course/Main Exercises/08_Toto-Numbers/solution.js
+++ Professional modules - (1)module/JS Core - (3)courses/JS Fundamentals - first course/Main Exercises/08_Toto-Numbers/solution.js
@@ -1,3 +1,28 @@
 function solve() {
-	//TODO
+	let button = document.querySelector("button");
+	button.addEventListener("click", display);
+
+	function display() {
+		let input = document.querySelector("input");
+		let inputArr = input.value.split(" ").map(x => +x);
+
+		if (inputArr.length === 6) {
+			
+		}else {
+			return;
+		}
+		for (let i = 1; i <= 49; i++) {
+
+			let allNumbers = document.getElementById("allNumbers");
+			let div = document.createElement("div");
+			if (inputArr.includes(i)) {
+				div.style.background = "orange";
+			}
+			div.setAttribute("class", "numbers");
+			div.textContent = i;
+			allNumbers.appendChild(div);
+		}
+		input.setAttribute("disabled", "");
+		button.setAttribute("disabled", "");
+	}
 }
