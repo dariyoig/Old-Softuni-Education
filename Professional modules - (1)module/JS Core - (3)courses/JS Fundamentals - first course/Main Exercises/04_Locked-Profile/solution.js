f58d4ad900aa61eb95afcdf843d9459d1a816c0c
Dariyo Ignatov <darioignatov72@gmail.com>
2019-01-23 14:07:47 +0200

    Solution added

--- Professional modules - (1)module/JS Core - (3)courses/JS Fundamentals - first course/Main Exercises/04_Locked-Profile/solution.js
+++ Professional modules - (1)module/JS Core - (3)courses/JS Fundamentals - first course/Main Exercises/04_Locked-Profile/solution.js
@@ -1,3 +1,22 @@
 function solve() {
-   //TODO
+    Array.from(document.getElementsByTagName('button')).forEach((el) => {
+        el.addEventListener('click', show);
+    })
+
+    function show(e) {
+        let parent = e.target.parentNode;
+        let input = parent.getElementsByTagName('input')[0];
+        let hiddenDiv = parent.getElementsByTagName('div')[1];
+        let isLocked = input.checked;
+        let button = e.target;
+        if (!isLocked && button.textContent === "Show more") {
+            hiddenDiv.style.display = "block";
+            button.textContent = "Hide";
+            //return
+        }else if (button.textContent === "Hide" && !isLocked){
+            hiddenDiv.style.display = "none";
+            button.textContent = "Show more";
+        }
+
+    }
 } 
