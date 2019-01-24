6ff959b01ca9708da28b4348d8b366c409160c51
Dariyo Ignatov <darioignatov72@gmail.com>
2019-01-24 10:39:49 +0200

    Solutions added

--- Professional modules - (1)module/JS Core - (3)courses/JS Fundamentals - first course/Main Exercises/07_Table-Search-Engine/solution.js
+++ Professional modules - (1)module/JS Core - (3)courses/JS Fundamentals - first course/Main Exercises/07_Table-Search-Engine/solution.js
@@ -1,3 +1,22 @@
 function solve() {
-    //TODO
+    function containsQuery(array, query) {
+        return array
+            .filter(x => x.textContent.toLowerCase().includes(query))
+            .map(x => array.indexOf(x));
+    }
+
+    let tdValues = [];
+    Array.from(document.querySelectorAll('tbody tr td'))
+        .forEach(x => tdValues.push(x));
+    Array.from(document.querySelectorAll('tbody tr td'))
+        .forEach((x, y) => x.setAttribute("id", y));
+    document.getElementById("searchBtn").addEventListener("click", (e) => {
+        Array.from(document.querySelectorAll("tbody tr"))
+            .forEach(x => x.removeAttribute("class"));
+        let searchQuery = document.getElementById("searchField").value.toLowerCase();
+        if (searchQuery.length === 0) return;
+        containsQuery(tdValues, searchQuery)
+            .forEach(x => tdValues[x].parentElement.setAttribute("class", "select"));
+        document.getElementById("searchField").value = "";
+    })
 }
