b302feb875173ade5817604928e558eecec0a50e
Dariyo Ignatov <darioignatov72@gmail.com>
2019-01-25 13:16:39 +0200

    Solutions Added

--- Professional modules - (1)module/JS Core - (3)courses/JS Fundamentals - first course/Labs/02_Even-Position-Element/solution.js
+++ Professional modules - (1)module/JS Core - (3)courses/JS Fundamentals - first course/Labs/02_Even-Position-Element/solution.js
@@ -1,3 +1,11 @@
 function solve() {
-  // TODO
+  let arr = JSON.parse(document.getElementById("arr").value);
+  let result = document.getElementById("result");
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (i % 2 === 0) {
+      newArr.push(arr[i]);
+    }
+  }
+  result.textContent = newArr.join(" x ")
 }
