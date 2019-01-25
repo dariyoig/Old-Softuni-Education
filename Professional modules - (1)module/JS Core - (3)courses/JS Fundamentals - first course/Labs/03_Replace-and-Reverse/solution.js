3ea90519676d1bc1822781051c6efeed65569803
Dariyo Ignatov <darioignatov72@gmail.com>
2019-01-25 15:06:29 +0200

    Solutions added

--- Professional modules - (1)module/JS Core - (3)courses/JS Fundamentals - first course/Labs/03_Replace-and-Reverse/solution.js
+++ Professional modules - (1)module/JS Core - (3)courses/JS Fundamentals - first course/Labs/03_Replace-and-Reverse/solution.js
@@ -1,3 +1,3 @@
 function solve() {
-  // TODO
+  document.getElementById("result").textContent = JSON.parse(document.getElementById("arr").value).map( x => x.split("").reverse().join("") ).map( y => y[0].toUpperCase() + y.slice(1)).join(" ");
 }
--- Professional modules - (1)module/JS Core - (3)courses/JS Fundamentals - first course/Labs/04_Find-Element/solution.js
+++ Professional modules - (1)module/JS Core - (3)courses/JS Fundamentals - first course/Labs/04_Find-Element/solution.js
@@ -1,3 +1,15 @@
 function solve() {
-  // TODO
+  let searched = Number(document.getElementById("num").value);
+  let arr = JSON.parse(document.getElementById("arr").value);
+  let result = document.getElementById("result");
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    let string = arr[i];
+    if (string.includes(searched)) {
+      newArr.push(`true -> ${string.indexOf(searched)}`)
+    }else {
+      newArr.push(`false -> -1`)
+    }
+  }
+  result.textContent = newArr.join(",");
 }
