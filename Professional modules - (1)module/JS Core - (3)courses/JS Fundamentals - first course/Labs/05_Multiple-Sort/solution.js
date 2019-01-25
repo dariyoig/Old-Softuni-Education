3e883b02fe09d12cd003a78c13b4fb2233cf40c9
Dariyo Ignatov <darioignatov72@gmail.com>
2019-01-25 15:31:48 +0200

    Solutions added

--- Professional modules - (1)module/JS Core - (3)courses/JS Fundamentals - first course/Labs/05_Multiple-Sort/solution.js
+++ Professional modules - (1)module/JS Core - (3)courses/JS Fundamentals - first course/Labs/05_Multiple-Sort/solution.js
@@ -1,3 +1,14 @@
 function solve() {
-  // TODO
+  let arr = JSON.parse(document.getElementById("arr").value);
+  let result = document.getElementById("result");
+  let p1 = document.createElement("div");
+  let p2 = document.createElement("div");
+  let ascendingOrder = arr.map(x => +x).sort((a, b) => a - b);
+  let alphaOrder = arr.sort();
+  console.log(ascendingOrder);
+  console.log(alphaOrder);
+  p1.textContent = ascendingOrder.join(", ");
+  result.appendChild(p1);
+  p2.textContent = alphaOrder.join(", ");
+  result.appendChild(p2);
 }
