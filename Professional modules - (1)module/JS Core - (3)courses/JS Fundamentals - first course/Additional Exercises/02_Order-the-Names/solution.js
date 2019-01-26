1e82fea9ae0faa68c14a357cd7534d1eb2f91ba0
Dariyo Ignatov <darioignatov72@gmail.com>
2019-01-26 19:41:57 +0200

    Solutions added

--- Professional modules - (1)module/JS Core - (3)courses/JS Fundamentals - first course/Additional Exercises/02_Order-the-Names/solution.js
+++ Professional modules - (1)module/JS Core - (3)courses/JS Fundamentals - first course/Additional Exercises/02_Order-the-Names/solution.js
@@ -1,11 +1,4 @@
 function solve(){
-    let zero = "0";
-    let a = ["5", "*", "1"]
-    let last = a.slice(a.length -1).join("");
-    a.splice(a.length -1, 1, last+zero)
 
-    console.log(a);
-    console.log(last);
-    console.log(a);
 }
 solve()
