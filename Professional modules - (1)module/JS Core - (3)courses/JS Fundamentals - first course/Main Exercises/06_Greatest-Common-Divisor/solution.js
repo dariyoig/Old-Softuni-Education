0cb919c3a6a5ce1e80f90ae654ae4b2f625193ff
Dariyo Ignatov <darioignatov72@gmail.com>
2019-01-26 15:22:56 +0200

    Solution added

--- Professional modules - (1)module/JS Core - (3)courses/JS Fundamentals - first course/Main Exercises/06_Greatest-Common-Divisor/solution.js
+++ Professional modules - (1)module/JS Core - (3)courses/JS Fundamentals - first course/Main Exercises/06_Greatest-Common-Divisor/solution.js
@@ -1,3 +1,21 @@
 function greatestCD() {
-    // TODO ...
+    let a = Number(document.getElementById("num1").value);
+    let b = Number(document.getElementById("num2").value);
+    let result = document.getElementById("result");
+
+    
+
+        function gcd(a,b) {
+            a = Math.abs(a);
+            b = Math.abs(b);
+            if (b > a) {var temp = a; a = b; b = temp;}
+            while (true) {
+                if (b == 0) return a;
+                a %= b;
+                if (a == 0) return b;
+                b %= a;
+            }
+        }
+        result.textContent = `${gcd(a, b)}`
+    
 }
