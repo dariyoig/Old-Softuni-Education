6f0dc6e7c8a1bab43eccd00d283e76988edcbbb0
Dariyo Ignatov <darioignatov72@gmail.com>
2019-01-26 20:04:22 +0200

    Solutions added

--- Professional modules - (1)module/JS Core - (3)courses/JS Fundamentals - first course/Labs/02_Temperature-Converter/solution.js
+++ Professional modules - (1)module/JS Core - (3)courses/JS Fundamentals - first course/Labs/02_Temperature-Converter/solution.js
@@ -1,3 +1,19 @@
 function solve() {
-  // TODO
+  let degrees = Number(document.getElementById("num1").value)
+  let string = document.getElementById("type").value.toLowerCase();
+  let result = document.getElementById("result");
+
+  switch (string) {
+    case 'celsius':
+      result.textContent = Math.round(((degrees * 9) / 5) + 32);
+      break;
+    case 'fahrenheit':
+      result.textContent = Math.round(((degrees - 32) * 5) /9);
+      break;
+
+    default: result.textContent = "Error!";
+      break;
+  }
+
+  console.log(string);
 }
