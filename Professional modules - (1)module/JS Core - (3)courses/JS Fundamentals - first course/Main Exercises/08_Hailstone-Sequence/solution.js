22628473f9be268e249141fbaa2a2b6151f2aee2
Dariyo Ignatov <darioignatov72@gmail.com>
2019-01-26 15:32:01 +0200

    Solution added

--- Professional modules - (1)module/JS Core - (3)courses/JS Fundamentals - first course/Main Exercises/08_Hailstone-Sequence/solution.js
+++ Professional modules - (1)module/JS Core - (3)courses/JS Fundamentals - first course/Main Exercises/08_Hailstone-Sequence/solution.js
@@ -1,3 +1,19 @@
 function getNext() {
-    // TODO ...
+    let number = Number(document.getElementById("num").value);
+    let result = document.getElementById("result");
+
+    function hailStoneSeq(n){
+        var seq=[n]
+    
+        while(n!=1){
+            if(n%2==0) n/=2
+            else n=(n*3)+1
+    
+            seq.push(n)
+        }
+    
+        return seq.join(' ')
+    }
+    
+    result.textContent = hailStoneSeq(number) + " ";
 }
