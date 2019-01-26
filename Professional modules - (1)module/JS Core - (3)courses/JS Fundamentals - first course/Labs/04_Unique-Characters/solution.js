48d58a9c0fedf58bbe945a97295416b157972a36
Dariyo Ignatov <darioignatov72@gmail.com>
2019-01-26 20:24:47 +0200

    Solutions added

--- Professional modules - (1)module/JS Core - (3)courses/JS Fundamentals - first course/Labs/04_Unique-Characters/solution.js
+++ Professional modules - (1)module/JS Core - (3)courses/JS Fundamentals - first course/Labs/04_Unique-Characters/solution.js
@@ -1,3 +1,18 @@
 function solve() {
-  // TODO
+  let string = document.getElementById("string").value;
+  string = string.split("");
+  let newStr = [];
+  for (let i = 0; i < string.length; i++) {
+    let current = string[i];
+    if (current === " ") {
+      newStr.push(current);
+
+    }else {
+      if (!newStr.includes(current)) {
+        newStr.push(current)
+      }
+    }
+
+  }
+  document.getElementById("result").textContent = newStr.join("");
 }
