a7937fe009b55265b1b97c2795611e59d0731784
Dariyo Ignatov <darioignatov72@gmail.com>
2019-01-26 20:12:31 +0200

    Solutions added

--- Professional modules - (1)module/JS Core - (3)courses/JS Fundamentals - first course/Labs/03_Count-Occurrences-of-a-Given-Character/solution.js
+++ Professional modules - (1)module/JS Core - (3)courses/JS Fundamentals - first course/Labs/03_Count-Occurrences-of-a-Given-Character/solution.js
@@ -1,3 +1,18 @@
 function solve() {
-  // TODO
+  let string = document.getElementById("string").value;
+  let char = document.getElementById("character").value;
+  let count = 0;
+  let print;
+  string.split("").forEach(x => {
+    if (x===char) {
+      count++;
+    }
+  })
+  console.log(count);
+  if (count % 2 ===0) {
+    print = `Count of ${char} is even.`;
+  }else {
+    print = `Count of ${char} is odd.`
+  }
+  document.getElementById("result").textContent = print;
 }
