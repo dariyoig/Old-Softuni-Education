2e83df9a661b6b9da14c14e74121beec9a7e7b62
Dariyo Ignatov <darioignatov72@gmail.com>
2019-01-26 15:58:02 +0200

    Solutions added

--- Professional modules - (1)module/JS Core - (3)courses/JS Fundamentals - first course/Main Exercises/07_Binary-Search/solution.js
+++ Professional modules - (1)module/JS Core - (3)courses/JS Fundamentals - first course/Main Exercises/07_Binary-Search/solution.js
@@ -1,3 +1,15 @@
 function binarySearch() {
-    // TODO ...
+    let number = document.getElementById("num").value;
+    let array = document.getElementById("arr").value.split(", ");
+    console.log(array);
+    let index = -1;
+    let result = document.getElementById("result");
+    let print = "";
+    if (array.includes(number)) {
+        index = array.indexOf(number);
+        print = `Found ${number} at index ${index}`
+    }else {
+        print = `${number} is not in the array`
+    }
+    result.textContent = print;
 }
--- Professional modules - (1)module/JS Core - (3)courses/JS Fundamentals - first course/Main Exercises/10_Factors/solution.js
+++ Professional modules - (1)module/JS Core - (3)courses/JS Fundamentals - first course/Main Exercises/10_Factors/solution.js
@@ -1,3 +1,10 @@
 function solve() {
-   // TODO ...
+    let num = Number(document.getElementById("num").value);
+    let result = document.getElementById("result");
+    const factors = number => Array
+        .from(Array(number + 1), (_, i) => i)
+        .filter(i => number % i === 0)
+
+    result.textContent = `${factors(num).join(" ")}`;
+    console.log(factors(num));
 }
