07fa5025b723bb8a382b2d595674430253cd0966
Dariyo Ignatov <darioignatov72@gmail.com>
2019-01-26 20:41:37 +0200

    Solutions added

--- Professional modules - (1)module/JS Core - (3)courses/JS Fundamentals - first course/Labs/05_Special-Words/solution.js
+++ Professional modules - (1)module/JS Core - (3)courses/JS Fundamentals - first course/Labs/05_Special-Words/solution.js
@@ -1,3 +1,25 @@
 function solve() {
-  // TODO
+  let num1 = Number(document.getElementById("firstNumber").value);
+  let num2 = Number(document.getElementById("secondNumber").value);
+  let tr1 = document.getElementById("firstString").value;
+  let tr2 = document.getElementById("secondString").value;
+  let tr3 = document.getElementById("thirdString").value;
+  let result = document.getElementById("result");
+
+  for (let i = num1; i <= num2; i++) {
+    let p = document.createElement("p");
+    if (i % 3 === 0 && i % 5 === 0) {
+      p.textContent = `${i} ${tr1}-${tr2}-${tr3}`;
+      result.appendChild(p);
+    } else if (i % 3 === 0) {
+      p.textContent = `${i} ${tr2}`;
+      result.appendChild(p);
+    } else if (i % 5 === 0) {
+      p.textContent = `${i} ${tr3}`;
+      result.appendChild(p);
+    } else {
+      p.textContent = `${i}`;
+      result.appendChild(p);
+    }
+  }
 }
