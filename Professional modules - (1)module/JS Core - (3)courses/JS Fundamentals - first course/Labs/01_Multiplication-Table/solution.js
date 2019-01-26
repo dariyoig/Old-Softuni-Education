bc1ae06b502dfde8696960388a95dca2928f8800
Dariyo Ignatov <darioignatov72@gmail.com>
2019-01-26 19:52:40 +0200

    Solutions added

--- Professional modules - (1)module/JS Core - (3)courses/JS Fundamentals - first course/Labs/01_Multiplication-Table/solution.js
+++ Professional modules - (1)module/JS Core - (3)courses/JS Fundamentals - first course/Labs/01_Multiplication-Table/solution.js
@@ -1,3 +1,17 @@
 function solve() {
-  // TODO
+  let num1 = Number(document.getElementById("num1").value);
+  let num2 = Number(document.getElementById("num2").value);
+  let result = document.getElementById("result");
+  let print;
+
+
+  if (num1 > num2) {
+    result.textContent = `Try with other numbers.`
+  } else {
+    for (let i = num1; i <= num2; i++) {
+      let p = document.createElement("p");
+      p.textContent = `${i} * ${num2} = ${i*num2}`;
+      result.appendChild(p);
+    }
+  }
 }
