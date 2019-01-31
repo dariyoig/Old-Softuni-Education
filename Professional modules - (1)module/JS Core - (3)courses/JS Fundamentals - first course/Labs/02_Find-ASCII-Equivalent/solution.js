c8302711b3ea74998603e2ceb7ed7c4ac0d7665e
Dariyo Ignatov <darioignatov72@gmail.com>
2019-01-31 10:44:30 +0200

    Solutions added

--- Professional modules - (1)module/JS Core - (3)courses/JS Fundamentals - first course/Labs/02_Find-ASCII-Equivalent/solution.js
+++ Professional modules - (1)module/JS Core - (3)courses/JS Fundamentals - first course/Labs/02_Find-ASCII-Equivalent/solution.js
@@ -1,3 +1,22 @@
 function solve() {
-  // TODO
+  let string = document.getElementById("str").value;
+  let resultEl = document.getElementById("result");
+  let chars = ""
+  string = string
+    .split(" ")
+    .forEach(x => {
+      if (isNaN(x)) {
+        let p = document.createElement("p");
+        let result = x.split("").map(y => y.charCodeAt()).join(" ");
+
+        p.textContent = result;
+        resultEl.appendChild(p);
+
+      } else {
+        chars += (String.fromCharCode(x));
+      }
+    })
+    let p =document.createElement("p")
+    p.textContent = chars;
+    resultEl.appendChild(p);
 }
