cd7b0e801149c4234c1fb5729802db18f4b9acc0
Dariyo Ignatov <darioignatov72@gmail.com>
2019-01-31 13:46:24 +0200

    More exercises added

--- Professional modules - (1)module/JS Core - (3)courses/JS Fundamentals - first course/Labs/03_Split-String-Equally/solution.js
+++ Professional modules - (1)module/JS Core - (3)courses/JS Fundamentals - first course/Labs/03_Split-String-Equally/solution.js
@@ -1,3 +1,16 @@
 function solve() {
-  // TODO
+  //let string = document.getElementById("str").value;
+  //let num = Number(document.getElementById("num").value);
+  //let resultEl = document.getElementById("result");
+  let result = [];
+
+  function split(str, num, arr) {
+    str = str.split("");
+    while (str.length > 0) {
+      
+    }
+    console.log(arr);
+  }
+  split("TestTestov1234", 2, [])
 }
+solve()
--- Professional modules - (1)module/JS Core - (3)courses/JS Fundamentals - first course/Labs/04_Replace-a-Certain-Word/solution.js
+++ Professional modules - (1)module/JS Core - (3)courses/JS Fundamentals - first course/Labs/04_Replace-a-Certain-Word/solution.js
@@ -1,3 +1,14 @@
 function solve() {
-  // TODO
+  let arr = JSON.parse(document.getElementById("arr").value);
+  let replacer = document.getElementById("str").value;
+  let length = arr[0].slice(7);
+  let regex = new RegExp(arr[0].slice(7).toLowerCase());
+
+  for(let el of arr){
+    let index = el.toLowerCase().indexOf(regex);
+    console.log(index);
+    console.log(length);
+  }
+
+  console.log(arr);
 }
