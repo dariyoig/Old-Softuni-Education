4480d106a7ea5ba5667976b2f010969be24c0995
Dariyo Ignatov <darioignatov72@gmail.com>
2019-02-02 19:16:22 +0200

    Solution Added

---
+++ Professional modules - (1)module/JS Core - (3)courses/JS Fundamentals - first course/Labs/countWordsInText.js
@@ -0,0 +1,15 @@
+function solve(input) {
+    input = input.join()
+    let arr = input.match(/[A-Za-z_]+/g);
+    let obj = {};
+    arr.forEach(element => {
+        if (obj.hasOwnProperty(element)) {
+            let currentCount = obj[element];
+            obj[element] = currentCount +1;
+        }else {
+            obj[element] = 1;
+        }
+    });
+    console.log(JSON.stringify(obj));
+}
+solve([ 'Far too slow, you\'re far too slow.' ]);
