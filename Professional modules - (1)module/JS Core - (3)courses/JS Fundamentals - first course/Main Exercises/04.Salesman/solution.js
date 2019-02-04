476fe7d1394e360cf35e114fb3fb4d85dca583f9
Dariyo Ignatov <darioignatov72@gmail.com>
2019-02-04 11:55:06 +0200

    Solution Added

--- Professional modules - (1)module/JS Core - (3)courses/JS Fundamentals - first course/Main Exercises/04.Salesman/solution.js
+++ Professional modules - (1)module/JS Core - (3)courses/JS Fundamentals - first course/Main Exercises/04.Salesman/solution.js
@@ -1,65 +1,61 @@
 function solve() {
+
   document.querySelector("button").addEventListener("click", loadProducts);
   document.querySelectorAll("#exercise button")[1].addEventListener("click", buyProducts);
   document.querySelectorAll("#exercise button")[2].addEventListener("click", getProfit);
+
   let textAreas = document.querySelectorAll("#exercise textarea");
-  let globalArr = [];
+  let storage = [];
   let profit = 0;
 
   function loadProducts(e) {
-    let arr = JSON.parse(textAreas[0].value);
+    let loadArr = JSON.parse(textAreas[0].value);
 
-    for (let product of arr) {
-      textAreas[2].value += `Successfully added ${product.quantity} ${product.name}. Price: ${product.price}\n`
-      globalArr.push(product);
+    for (let newProduct of loadArr) {
+      let exists = false;
+      for (let product of storage) {
+        if (product.name === newProduct.name) {
+          exists = true;
+          let currentQuantity = product.quantity;
+          product.quantity = currentQuantity + newProduct.quantity;
+          product.price = newProduct.price;
+        }
+      }
+      if (!exists) {
+        storage.push(newProduct);
+      }
+      textAreas[2].value += `Successfully added ${newProduct.quantity} ${newProduct.name}. Price: ${newProduct.price}\n`
     }
   }
 
   function buyProducts(e) {
 
-    let obj = JSON.parse(textAreas[1].value);
+    let order = JSON.parse(textAreas[1].value);
     let result = "";
-    if (globalArr.length == 0) {
-      result = "Cannot complete order.\n";
-      return;
-    }
-    for (let product of globalArr) {
-      if (product.name === obj.name) {
-        if (product.quantity >= obj.quantity) {
-          product.quantity = product.quantity - obj.quantity;
-          profit += obj.quantity * product.price;
-          result = `${obj.quantity} ${obj.name} sold for ${profit}.\n`
+
+    for (let product of storage) {
+      if (product.name === order.name) {
+        if (product.quantity >= order.quantity) {
+          let boughtFor = 0;
+          product.quantity = product.quantity - order.quantity;
+          profit += order.quantity * product.price;
+          boughtFor = order.quantity * product.price;
+          result = `${order.quantity} ${order.name} sold for ${boughtFor}.\n`;
+          break;
 
         } else {
           result = "Cannot complete order.\n";
-
         }
+      } else {
+        result = "Cannot complete order.\n";
       }
     }
-
     textAreas[2].value += result;
   }
 
+
   function getProfit(e) {
-    textAreas[2].value += `Profit: ${profit.toFixed(2)}`;
-    Array.from(document.querySelectorAll("button")).map(x => x.disabled = true);
+    textAreas[2].value += `Profit: ${profit.toFixed(2)}\n`;
+    loadArray.from(document.querySelectorAll("button")).map(x => x.disabled = true);
   }
 }
-
-[
-  {
-    "name": "tomatoes",
-    "quantity": 20,
-    "price": 0.50
-  },
-  {
-    "name": "potatoes",
-    "quantity": 10,
-    "price": 0.60
-  }
-]
-// {
-//   "name": "tomatoes",
-//   "quantity": 20
-
-// }
