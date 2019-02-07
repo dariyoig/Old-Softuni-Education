590dab212ff22e60bcf56581ac0d30bc1642a97c
Dariyo Ignatov <darioignatov72@gmail.com>
2019-02-07 19:41:06 +0200

    Solution Added

--- Professional modules - (1)module/JS Core - (3)courses/JS Fundamentals - first course/Exam Preparation/FirstProblem-Stadium/solution.js
+++ Professional modules - (1)module/JS Core - (3)courses/JS Fundamentals - first course/Exam Preparation/FirstProblem-Stadium/solution.js
@@ -2,16 +2,15 @@ function solve() {
     Array.from(document.querySelectorAll("button.seat")).forEach(e => e.addEventListener("click", checkSeat));
     document.querySelector("#summary button").addEventListener("click", getMoneyAndFans);
     
-    let profit = 0;
-    let numOfFans = 0;
+    let [profit, numOfFans] = [0, 0];
     let prices = {
         "Litex": [10,7,5],
         "Levski": [10,7,5],
         "VIP": [25, 15,10] 
     }
     function checkSeat(e) {
-        let output = document.getElementsByTagName('textarea')[0];
-        let section = e.target.parentNode.parentNode.parentNode.parentNode.parentNode.querySelector("h2").textContent;
+        let output = document.getElementById("output");
+        let section = e.path[5].className;
         let button = e.target;
         let seatNumber = e.target.textContent;
         let td = button.parentNode;
@@ -19,16 +18,16 @@ function solve() {
         let sectors = ["A", "B", "C"];
         if (!button.hasAttribute("style")) {
             button.style.backgroundColor = "rgb(255,0,0)";
-            output.textContent += ` Seat ${seatNumber} in zone ${section} sector ${sectors[+index]} was taken.\n`;
+            output.textContent += ` Seat ${seatNumber} in zone ${section} sector ${sectors[index]} was taken.\n`;
             numOfFans++;
             profit += prices[section][index];
         } else {
-            output.textContent += ` Seat ${seatNumber} in zone ${section} sector ${sectors[+index]} is unavailable.\n`
+            output.textContent += ` Seat ${seatNumber} in zone ${section} sector ${sectors[index]} is unavailable.\n`;
         }
        
     }
     function getMoneyAndFans(e) {
         let summarySpan = document.querySelector("#summary span");
-        summarySpan.textContent = `${+profit} leva, ${numOfFans} fans.`
+        summarySpan.textContent = `${profit} leva, ${numOfFans} fans.`;
     }
 }
