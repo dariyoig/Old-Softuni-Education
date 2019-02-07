d524f3f185d41e5aec1946f24c3ad00dd7db0954
Dariyo Ignatov <darioignatov72@gmail.com>
2019-02-07 13:16:10 +0200

    Solution Added

--- Professional modules - (1)module/JS Core - (3)courses/JS Fundamentals - first course/Exam Preparation/FirstProblem-Stadium/solution.js
+++ Professional modules - (1)module/JS Core - (3)courses/JS Fundamentals - first course/Exam Preparation/FirstProblem-Stadium/solution.js
@@ -1,3 +1,47 @@
 function solve() {
-    // TODO
+    Array.from(document.querySelectorAll("button.seat")).forEach(e => e.addEventListener("click", checkSeat));
+    document.querySelector("#summary button").addEventListener("click", getMoneyAndFans);
+    
+    let profit = 0;
+    let numOfFans = 0;
+    let prices = {
+        "Litex": {
+            "0": 10,
+            "1": 7,
+            "2": 5
+        },
+        "Levski": {
+            "0": 10,
+            "1": 7,
+            "2": 5
+        },
+        "VIP": {
+            "0": 25,
+            "1": 15,
+            "2": 10
+        }
+    }
+    function checkSeat(e) {
+        let outputArea = document.getElementById("output");
+        let section = e.target.parentNode.parentNode.parentNode.parentNode.parentNode.querySelector("h2").textContent;
+        let button = e.target;
+        let seatNumber = e.target.textContent;
+        let td = button.parentNode;
+        let index = [...td.parentNode.children].indexOf(td).toString();
+        let sectors = ["A", "B", "C"];
+        if (button.hasAttribute("style")) {
+            outputArea.textContent += ` Seat ${seatNumber} in zone ${section} sector ${sectors[+index]} is unavailable.\n`
+        } else {
+            button.style.backgroundColor = "rgb(255,0,0)";
+            outputArea.textContent += ` Seat ${seatNumber} in zone ${section} sector ${sectors[+index]} was taken.\n`;
+            numOfFans++;
+            profit += prices[section][index];
+            
+        }
+       
+    }
+    function getMoneyAndFans(e) {
+        let summarySpan = document.querySelector("#summary span");
+        summarySpan.textContent = `${+profit} leva, ${numOfFans} fans.`
+    }
 }
