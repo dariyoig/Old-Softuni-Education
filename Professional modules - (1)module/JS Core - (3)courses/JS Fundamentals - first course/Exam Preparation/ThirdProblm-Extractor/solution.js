e6e3617c9eee1bf6e054d16ec9eecc02deb3f928
Dariyo Ignatov <darioignatov72@gmail.com>
2019-02-07 18:19:41 +0200

    Solution Added

--- Professional modules - (1)module/JS Core - (3)courses/JS Fundamentals - first course/Exam Preparation/ThirdProblm-Extractor/solution.js
+++ Professional modules - (1)module/JS Core - (3)courses/JS Fundamentals - first course/Exam Preparation/ThirdProblm-Extractor/solution.js
@@ -1,3 +1,24 @@
 function solve() {
-    // TODO
+    document.querySelector("button").addEventListener("click", convert);
+
+    function convert(e) {
+        const escapeRegExp = (string) => {
+            return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
+          }
+        let input = document.getElementById("input").value;
+        let output = document.getElementById("output");
+        let firstNum = input.match(/[0-9]+/g)[0];
+        input = input.replace(firstNum, "");
+        let string = input.substr(0, +firstNum);
+        let lastChar = string[string.length - 1];
+        let escaped = escapeRegExp(lastChar);
+        let reg = new RegExp(escaped,"g");
+        let [partOne, partTwo] = string.split(reg);
+        let  re = new RegExp("["+partOne+"]","g");
+        partTwo = partTwo.replace(re, "");
+        while(partTwo.includes("#")){
+            partTwo = partTwo.replace("#", " ");
+        }
+        output.textContent = partTwo
+    }
 }
