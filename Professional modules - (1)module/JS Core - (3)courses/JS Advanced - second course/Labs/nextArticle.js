4f1febdf5f7f4c3a5cc7e8ffe99253c375838afa
Dariyoig <darioignatov72@gmail.com>
2019-02-21 00:23:51 +0200

    Update nextArticle.js

---
+++ Professional modules - (1)module/JS Core - (3)courses/JS Advanced - second course/Labs/nextArticle.js
@@ -0,0 +1,10 @@
+function getArticleGenerator(articles) {
+  return function () {
+    if (articles.length > 0) {
+      let div = document.getElementById("content");
+      let article = document.createElement("article");
+      article.textContent = articles.shift();
+      div.appendChild(article);
+    }
+  }
+}
