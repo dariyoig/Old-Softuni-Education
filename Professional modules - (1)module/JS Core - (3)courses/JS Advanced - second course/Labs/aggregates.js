c8320743d04bfe6e8d3fae3471634e7a3e54967d
Dariyoig <darioignatov72@gmail.com>
2019-02-20 21:59:26 +0200

    Update aggregates.js

--- Professional modules - (1)module/JS Core - (3)courses/JS Advanced - second course/Labs/aggregates.js
+++ Professional modules - (1)module/JS Core - (3)courses/JS Advanced - second course/Labs/aggregates.js
@@ -1,6 +1,9 @@
 function solve(input) {
     console.log(`Sum = ${input.reduce((acc, c) => acc+c, 0)}`);
-
+    console.log(`Min = ${Math.min(...input)}`);
+    console.log(`Max = ${Math.max(...input)}`);
+    console.log(`Product = ${input.reduce((acc, c) => acc*c)}`);
+    console.log(`Join = ${input.join("")}`);
 }
 solve(
     [2, 3, 10, 5]
