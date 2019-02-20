422cc413d6c0626b643c6db7f5f306f2e2d5e9ec
Dariyoig <darioignatov72@gmail.com>
2019-02-20 23:44:03 +0200

    Update maxEl.js

--- Professional modules - (1)module/JS Core - (3)courses/JS Advanced - second course/Labs/maxEl.js
+++ Professional modules - (1)module/JS Core - (3)courses/JS Advanced - second course/Labs/maxEl.js
@@ -1,3 +1,15 @@
 function solve(input) {
     return Math.max(...input);
 }
+//OR
+function solve(input) {
+    return input.reduce((acc, c) => Math.max(acc, c));
+}
+//OR
+function solve(input) {
+    return input.sort((a, b) => b-a)[0];
+}
+//OR
+function solve(input) {
+    return Math.max.apply(null, input);
+}
