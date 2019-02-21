240697bd066eb2ce78335da7a59665a4121c71d7
Dariyoig <darioignatov72@gmail.com>
2019-02-21 12:08:11 +0200

    Update sortArr.js

--- Professional modules - (1)module/JS Core - (3)courses/JS Advanced - second course/Main Exercises/sortArr.js
+++ Professional modules - (1)module/JS Core - (3)courses/JS Advanced - second course/Main Exercises/sortArr.js
@@ -1,7 +1,6 @@
 function solve(arr, command) {
-    if (command === "asc") {
-        return arr.sort((a, b) => a - b);
-    } else {
-        return arr.sort((a, b) => b - a)
+    switch (command) {
+        case 'asc': return arr.sort((a, b) => a - b);
+        default: return arr.sort((a, b) => b - a)
     }
 }
