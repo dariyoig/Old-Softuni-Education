1001dba79d16010f5946ee9843b38927ce46d89b
Dariyoig <darioignatov72@gmail.com>
2019-02-21 14:10:10 +0200

    Update functionalSum.js

---
+++ Professional modules - (1)module/JS Core - (3)courses/JS Advanced - second course/Main Exercises/functionalSum.js
@@ -0,0 +1,11 @@
+let add = (function () {
+    let sum = 0;
+    function add(value) {
+        sum += value;
+        return add;
+    }
+    add.toString = function(){
+        return sum;
+    }
+    return add;
+})();
